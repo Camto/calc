63b498b730dc351c2a35a58beb4950f6e51a220a
Benjamin Philippe Applegate <www.ben.pr@gmail.com>
2018-06-08 12:48:47 -0400

    COPY PASTY

--- calc/run part.js
+++ calc/run part.js
@@ -172,7 +172,81 @@ function get_variable(name, scopes) {
 }
 
 function run_block(block, scope) {
-	
+	for(let instruccion_pointer = 0; instruccion_pointer < ast.data.length; instruccion_pointer++) {
+		if(Date.now() > end_time) {
+			throw "Error: code took too long to run, stopped.";
+		}
+		
+		switch(ast.data[instruccion_pointer].type) {
+			case "symbol":
+				if(variables[ast.data[instruccion_pointer].data]) {
+					switch(variables[ast.data[instruccion_pointer].data].type) {
+						case "function":
+							run_function(variables[ast.data[instruccion_pointer].data], stack, built_ins, operators, end_time);
+							break;
+						case "symbol":
+							built_ins[variables[ast.data[instruccion_pointer].data].data]();
+							break;
+						case "operator":
+							operators[variables[ast.data[instruccion_pointer].data].data]();
+							break;
+						default:
+							stack.push(variables[ast.data[instruccion_pointer].data]);
+							break;
+					}
+				} else if(built_ins[ast.data[instruccion_pointer].data]) {
+					built_ins[ast.data[instruccion_pointer].data]();
+				} else {
+					throw `Symbol \`${ast.data[instruccion_pointer].data}\` found in main expression without being a built-in function.`;
+				}
+				break;
+			case "number":
+			case "string":
+				stack.push(ast.data[instruccion_pointer]);
+				break;
+			case "list":
+				var list = [];
+				for(let cou = 0; cou < ast.data[instruccion_pointer].data; cou++) {
+					list.push(stack.pop());
+				}
+				stack.push({data: list.reverse(), type: "list"});
+				break;
+			case "function":
+				var scoped_function = ast.data[instruccion_pointer];
+				scoped_function.scopes = [variables];
+				stack.push(scoped_function);
+				break;
+			case "operator":
+				if(ast.data[instruccion_pointer].data != "$") {
+					operators[ast.data[instruccion_pointer].data]();
+				} else {
+					instruccion_pointer++;
+					switch(ast.data[instruccion_pointer].type) {
+						case "symbol":
+							if(variables[ast.data[instruccion_pointer].data]) {
+								var passed_function = variables[ast.data[instruccion_pointer].data];
+								passed_function.name = ast.data[instruccion_pointer].data;
+								passed_function.scopes = [variables];
+								passed_function.is_ref = true;
+								stack.push(passed_function);
+							} else {
+								stack.push(ast.data[instruccion_pointer]);
+							}
+							break;
+						case "operator":
+							stack.push(ast.data[instruccion_pointer]);
+							break;
+						default:
+							var reference = ast.data[instruccion_pointer];
+							reference.scopes = [variables];
+							reference.is_ref = true;
+							stack.push(reference);
+							break;
+					}
+				}
+				break;
+		}
+	}
 }
 
 try {
