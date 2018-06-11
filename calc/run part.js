bdc4dc9496c5b9e6b2e711b5cd6cd62ab96f4ccd
Benjamin Philippe Applegate <www.ben.pr@gmail.com>
2018-06-11 14:20:05 -0400

    Yay more `run_block`.

--- calc/run part.js
+++ calc/run part.js
@@ -15,67 +15,7 @@ function run_function(func, stack, built_ins, operators, end_time) {
 					variables[func.variables[cou].name] = stack.pop();
 				}
 				
-				var code = func.data;
-				for(let code_pointer = 0; code_pointer < code.length; code_pointer++) {
-					if(Date.now() > end_time) {
-						throw "Error: code took too long to run, stopped.";
-					}
-					
-					switch(code[code_pointer].type) {
-						case "symbol":
-							if(!built_ins[code[code_pointer].data]) {
-								if(get_variable(code[code_pointer].data, scopes)) {
-									switch(get_variable(code[code_pointer].data, scopes).type) {
-										case "function":
-											run_function(get_variable(code[code_pointer].data, scopes), stack, built_ins, operators, end_time);
-											break;
-										case "symbol":
-											built_ins[get_variable(code[code_pointer].data, scopes).data]();
-											break;
-										case "operator":
-											operators[get_variable(code[code_pointer].data, scopes).data]();
-											break;
-										default:
-											stack.push(get_variable(code[code_pointer].data, scopes));
-											break;
-									}
-								} else {
-									throw `Symbol \`${code[code_pointer].data}\` found in function without being a built-in function or a function parameter.`;
-								}
-							} else {
-								built_ins[code[code_pointer].data]();
-							}
-							break;
-						case "number":
-						case "string":
-							stack.push(code[code_pointer]);
-							break;
-						case "function":
-							var scoped_function = code[code_pointer];
-							scoped_function.scopes = func.scopes.concat(args, variables);
-							stack.push(scoped_function);
-							break;
-						case "list":
-							var list = [];
-							for(let cou = 0; cou < code[code_pointer].data; cou++) {
-								list.push(stack.pop());
-							}
-							stack.push({data: list.reverse(), type: "list"});
-							break;
-						case "operator":
-							if(code[code_pointer].data != "$") {
-								operators[code[code_pointer].data]();
-							} else {
-								code_pointer++;
-								if(!args[code[code_pointer].data]) {
-									stack.push(code[code_pointer]);
-								} else {
-									stack.push(args[code[code_pointer].data]);
-								}
-							}
-							break;
-					}
-				}
+				run_block(func.data, stack, scopes, built_ins, operators, end_time);
 				break;
 			case "symbol":
 				built_ins[func.data]();
