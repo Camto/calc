f2e953d70c124084c62696942b607c7ec4b320ed
Benjamin Philippe Applegate <www.ben.pr@gmail.com>
2018-12-22 03:03:04 -0400

    Changing code from ` to ".

--- calc/calc.js
+++ calc/calc.js
@@ -6,7 +6,7 @@ var run = require("./run");
 
 function calc(code_, max_time) {
 	if(code_.substr(0, 5) != "calc=") {
-		throw "There is no `calc=`.";
+		throw "There is no \"calc=\".";
 	}
 	var code = code_.substr(5, code_.length);
 	var tokens = lex(code);
