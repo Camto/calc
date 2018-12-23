ca6b9231d91cea4551a684e4a79a0243fffa9570
Benjamin Philippe Applegate <www.ben.pr@gmail.com>
2018-12-23 03:59:23 -0400

    Log bot into #tst because #proglangdesign needs registration.

--- irc/calcbot.js
+++ irc/calcbot.js
@@ -4,25 +4,25 @@ var calc = require("./../calc/calc");
 
 var irc = require("irc");
 
-var configuration = {
-	channels: ["#proglangdesign"],
+var config = {
+	channels: ["#tst"],
 	server: "irc.freenode.net",
 	name: "calcbot"
 };
 
-var bot = new irc.Client(configuration.server, configuration.name, {
-	channels: configuration.channels
+var bot = new irc.Client(config.server, config.name, {
+	channels: config.channels
 });
 
 bot.addListener("message", function(from, to, text, message) {
 	if (from != "calcbot" && text.substring(0, 5) == "calc=") {
-		bot.say(configuration.channels[0], (() => {
+		bot.say(config.channels[0], (() => {
 			try {
 				return calc.print(calc.calc(text));
 			} catch(err) {
-				console.log(err);
-				return err.toString();
+				console.log(calc.err_to_str(err));
+				return calc.err_to_str(err);
 			}
-		})());
+		})().replace("\t", "    "));
 	}
 });
