d88749aee8af1a0a5d6d7856ff23a59a223a7694
Benjamin Philippe Applegate <www.ben.pr@gmail.com>
2018-12-24 23:55:45 -0400

    Move the help pages to their own file.
    They were taking up too much space in the standard library file. If this file gets too big it might get split by category.

--- calc.js
+++ calc.js
@@ -1,4 +1,303 @@
 require=(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+module.exports = {
+	help: aliases => `
+
+	HELP MENU
+
+Usage: "calc= help"
+
+Aliases: ${aliases}.
+
+It shows the main calc= menu.
+`,
+	page: aliases => `
+
+	BUILT-IN/OPERATOR DOCUMENTATION
+
+Usage: "calc= page" or "calc= $command page", where "command" is the name of a built-in or an operator.
+
+Aliases: ${aliases}.
+
+Examples:
+	* "calc= $+ page"
+	* "calc= $page page"
+
+Without any arguments, it shows how to use the documentation. With a symbol or operator, it shows the help page of the built-in or operator.
+`,
+	tut: aliases => `
+
+	BASIC TUTORIAL
+
+Usage: "calc= tut" or "calc= n tut", where "n" is the tutorial page number.
+
+Aliases: ${aliases}.
+
+It shows the basic tutorial page.
+`,
+	adv_tut: aliases => `
+
+	ADVANCED TUTORIAL
+
+Usage: "calc= adv_tut" or "calc= n adv_tut", where "n" is the tutorial page number.
+
+Aliases: ${aliases}.
+
+It shows the advanced tutorial page.
+`,
+	type: aliases => `
+
+	TYPE
+
+Usage: "calc= value type", where "value" is any value.
+
+Aliases: ${aliases}.
+
+Examples:
+	* "calc= 1 type" -> "calc=number"
+	* "calc= {} type" -> "calc=function"
+
+Returns the type of any given value. These can be: number, string, list, function, symbol, or operator.
+`,
+	"true": aliases => `
+
+	TRUE
+
+Usage: "calc= true".
+
+Aliases: ${aliases}.
+
+Examples:
+	* "calc= true" -> "calc=1"
+
+Returns the simplest truthy value, 1.
+`,
+	"false": aliases => `
+
+	FALSE
+
+Usage: "calc= false".
+
+Aliases: ${aliases}.
+
+Examples:
+	* "calc= false" -> "calc=0"
+
+Returns the simplest falsy value, 0.
+`,
+	"if": aliases => `
+
+	IF/ELSE STATEMENT
+
+Usage: "calc= bool if_true if_false if", where "bool" is a truthy of falsy value, "if_true" is the branch for if the condition is true, and "if_false" is the alternate branch.
+
+Aliases: ${aliases}.
+
+Examples:
+	* "calc= 3 true {1+} {1-} if" -> "calc=4"
+	* "calc= 3 0 {1+} {1-} if" -> "calc=2"
+	* "calc= "oigb" $5 $3 if" -> "calc=5"
+	* "calc= false $5 $3 if" -> "calc=3"
+
+If the condition is true, it executes the first branch, if not, then it executes the other branch. 
+
+For different types, there are different ways to test if it is truthy:
+	* Numbers that are 0 are falsy others are truthy.
+	* String and Lists are treated as a number of their length, emptiness being 0, or falsy.
+	* Functions, symbols, and operators are all truthy.
+`,
+	dup: aliases => `
+
+	DUPLICATE
+
+Usage: "calc= a dup", where "a" is any value.
+
+Aliases: ${aliases}.
+
+Examples:
+	* "calc= 5 dup" -> "calc=5 5"
+	* "calc= "str" "str" dup" -> "calc=str str"
+
+It duplicates the top value of the stack. You can use dup to use some value without discarding it.
+`,
+	swap: aliases => `
+
+	SWAP
+
+Usage: "calc= a b swap", where "a" and "b" can be any value.
+
+Aliases: ${aliases}.
+
+Examples:
+	* "calc= 2 5 swap" -> "calc=5 2"
+	* "calc= "gh" [1, 2, 3] swap" -> "calc=[1, 2, 3] gh"
+
+It swaps the top 2 values of the stack. This means you can use a value underneath another.
+`,
+	stack_reversen: aliases => `
+
+	REVERSE TOP N ITEMS
+
+${built_in_warning}
+
+Usage: "calc= n ... m num stack_reversen", where "num" is the amount of items you want to reverse and "n ... m" are the items.
+
+Aliases: ${aliases}.
+
+Examples:
+	* "calc= 'a 'd 'y 'f 4 stack_reversen" -> "calc=f y d a"
+	* "calc= 10 6 .. expl 1 5 .. expl 10 stack_reversen" -> "calc= 5 4 3 2 1 6 7 8 9 10"
+
+It reverses the top num items of the stack.
+`,
+	drop: aliases => `
+
+	DROP
+
+Usage: "calc= a drop", where "a" is any value.
+
+Aliases: ${aliases}.
+
+Examples:
+	* "calc= 1 2 3 drop" -> "calc=1 2"
+	* "calc= 'a 'b [] drop" -> "calc=a b"
+
+It removes the top item of the stack.
+`,
+	dropn: aliases => `
+
+	DROP N ITEMS
+
+${built_in_warning}
+
+Usage: "calc= n ... m num dropn", where "num" is the amount of items you want to drop and "n ... m" are the items.
+
+Aliases: ${aliases}.
+
+Examples:
+	* "calc= 'a 'b 'c 'd 2 dropn" -> "calc=a b"
+	* "calc= 10 1 .. expl 5 dropn" -> "calc=10 9 8 7 6"
+
+It drops the top num items of the stack. It should be called with a known, fixed number or you could remove the wrong items, which would cause very strange bugs.
+`,
+	rot: aliases => `
+
+	ROTATE
+
+Usage: "calc= a b c rot", where "a", "b", and "c" can be any value.
+
+Aliases: ${aliases}.
+
+Examples:
+	* "calc= 'a 'b 'c rot" -> "calc=c a b"
+	* "calc= "as" [] 1 rot" -> "calc=1 as []"
+
+It rotates the top three items of the stack to the right. To do this with more items you would use roll, but it is not recommended.
+`,
+	unrot: aliases => `
+
+	ROTATE BACKWARD
+
+Usage: "calc= a b c unrot", where "a", "b", and "c" can be any value.
+
+Aliases: ${aliases}.
+
+Examples:
+	* "calc= 'a 'b 'c unrot" -> "calc=b c a"
+	* "calc= "as" [] 1 unrot" -> "calc=[] 1 as"
+
+It rotates the top three items of the stack to the left. To do this with more items you would use unroll, but it is not recommended.
+`,
+	roll: aliases => `
+
+	ROLL
+
+Usage: "calc= n ... m num roll", where "num" is the amount of items you want to roll and "n ... m" are the items.
+
+Aliases: ${aliases}.
+
+Examples:
+	* "calc= 'a 'b 'c 'd 'e 'f 4 roll" -> "calc=a b f c d e"
+	* "calc= 1 5 .. {n -> 1 n .. expl n roll n group} map" -> "calc=[[1], [2, 1], [3, 1, 2], [4, 1, 2, 3], [5, 1, 2, 3, 4]]"
+
+It rotates the top num items of the stack to the left. If you want to roll 2 or 3 three items, please check out swap and rot.
+`,
+	unroll: aliases => `
+
+	ROLL BACKWARD
+
+Usage: "calc= n ... m num unroll", where "num" is the amount of items you want to roll backward and "n ... m" are the items.
+
+Aliases: ${aliases}.
+
+Examples:
+	* "calc= 'a 'b 'c 'd 'e 'f 4 unroll" -> "calc=a b d e f c"
+	* "calc= 1 5 .. {n -> 1 n .. expl n unroll n group} map" -> "calc=[[1], [2, 1], [2, 3, 1], [2, 3, 4, 1], [2, 3, 4, 5, 1]]"
+
+It rotates the top num items of the stack to the right. If you want to roll backward 2 or 3 three items, please check out swap and unrot.
+`,
+/*
+	nip
+	tuck
+	over
+	map
+	fold
+	foldr
+	filter
+	length
+	head
+	snd
+	last
+	back_snd
+	nth
+	back_nth
+	init
+	tail
+	body
+	reverse
+	reversen
+	popn
+	expl
+	group
+	copy_group
+	group_all
+	copy_group_all
+	list_dup
+	list_swap
+	list_rot
+	list_unrot
+	list_roll
+	list_reverse_roll
+	list_nip
+	list_tuck
+	list_over
+	pi
+	e
+	abs
+	round
+	ceil
+	floor
+	max
+	main
+	sgn
+	rand
+	cos
+	sin
+	tan
+	sqrt
+	cbrt
+	root
+	log
+	call
+	iter
+	id
+	dot
+	set
+	inc
+	dec*/
+};
+
+var built_in_warning = "!WARNING: This function is discouraged from being used, the only reason it is here is for the few cases in which it is necessary!";
+},{}],2:[function(require,module,exports){
 "use strict";
 
 var types = require("./types");
@@ -120,7 +419,7 @@ function lex(code) {
 }
 
 module.exports = lex;
-},{"./types":7}],2:[function(require,module,exports){
+},{"./types":8}],3:[function(require,module,exports){
 "use strict";
 
 var types = require("./types");
@@ -279,7 +578,7 @@ function parse(tokens) {
 var is_op = (token, op) => token.data == op && token.type == types.op;
 
 module.exports = parse;
-},{"./types":7}],3:[function(require,module,exports){
+},{"./types":8}],4:[function(require,module,exports){
 var types = require("./types");
 
 module.exports = function print(value) {
@@ -320,7 +619,7 @@ module.exports = function print(value) {
 		
 	}
 }
-},{"./types":7}],4:[function(require,module,exports){
+},{"./types":8}],5:[function(require,module,exports){
 "use strict";
 
 var types = require("./types");
@@ -434,7 +733,7 @@ function run_block(block, stack, scopes, built_ins, operators, end_time) {
 }
 
 module.exports = {run_function, run_block};
-},{"./types":7,"./variable manipulation":8}],5:[function(require,module,exports){
+},{"./types":8,"./variable manipulation":9}],6:[function(require,module,exports){
 "use strict";
 
 var run_part = require("./run part");	
@@ -460,13 +759,14 @@ function run(ast, max_time = Infinity) {
 }
 
 module.exports = run;
-},{"./run part":4,"./standard library":6}],6:[function(require,module,exports){
+},{"./run part":5,"./standard library":7}],7:[function(require,module,exports){
 "use strict";
 
 var types = require("./types");
 var print = require("./print");
 var run_part = require("./run part");
 var variable_manipulation = require("./variable manipulation");
+var help_pages = require("./help pages");
 
 // Generate built-ins based on a stack, operators, and an end time.
 
@@ -1323,305 +1623,6 @@ function operators(stack) {
 	};
 }
 
-var help_pages = {
-	help: aliases => `
-
-	HELP MENU
-
-Usage: "calc= help"
-
-Aliases: ${aliases}.
-
-It shows the main calc= menu.
-`,
-	page: aliases => `
-
-	BUILT-IN/OPERATOR DOCUMENTATION
-
-Usage: "calc= page" or "calc= $command page", where "command" is the name of a built-in or an operator.
-
-Aliases: ${aliases}.
-
-Examples:
-	* "calc= $+ page"
-	* "calc= $page page"
-
-Without any arguments, it shows how to use the documentation. With a symbol or operator, it shows the help page of the built-in or operator.
-`,
-	tut: aliases => `
-
-	BASIC TUTORIAL
-
-Usage: "calc= tut" or "calc= n tut", where "n" is the tutorial page number.
-
-Aliases: ${aliases}.
-
-It shows the basic tutorial page.
-`,
-	adv_tut: aliases => `
-
-	ADVANCED TUTORIAL
-
-Usage: "calc= adv_tut" or "calc= n adv_tut", where "n" is the tutorial page number.
-
-Aliases: ${aliases}.
-
-It shows the advanced tutorial page.
-`,
-	type: aliases => `
-
-	TYPE
-
-Usage: "calc= value type", where "value" is any value.
-
-Aliases: ${aliases}.
-
-Examples:
-	* "calc= 1 type" -> "calc=number"
-	* "calc= {} type" -> "calc=function"
-
-Returns the type of any given value. These can be: number, string, list, function, symbol, or operator.
-`,
-	"true": aliases => `
-
-	TRUE
-
-Usage: "calc= true".
-
-Aliases: ${aliases}.
-
-Examples:
-	* "calc= true" -> "calc=1"
-
-Returns the simplest truthy value, 1.
-`,
-	"false": aliases => `
-
-	FALSE
-
-Usage: "calc= false".
-
-Aliases: ${aliases}.
-
-Examples:
-	* "calc= false" -> "calc=0"
-
-Returns the simplest falsy value, 0.
-`,
-	"if": aliases => `
-
-	IF/ELSE STATEMENT
-
-Usage: "calc= bool if_true if_false if", where "bool" is a truthy of falsy value, "if_true" is the branch for if the condition is true, and "if_false" is the alternate branch.
-
-Aliases: ${aliases}.
-
-Examples:
-	* "calc= 3 true {1+} {1-} if" -> "calc=4"
-	* "calc= 3 0 {1+} {1-} if" -> "calc=2"
-	* "calc= "oigb" $5 $3 if" -> "calc=5"
-	* "calc= false $5 $3 if" -> "calc=3"
-
-If the condition is true, it executes the first branch, if not, then it executes the other branch. 
-
-For different types, there are different ways to test if it is truthy:
-	* Numbers that are 0 are falsy others are truthy.
-	* String and Lists are treated as a number of their length, emptiness being 0, or falsy.
-	* Functions, symbols, and operators are all truthy.
-`,
-	dup: aliases => `
-
-	DUPLICATE
-
-Usage: "calc= a dup", where "a" is any value.
-
-Aliases: ${aliases}.
-
-Examples:
-	* "calc= 5 dup" -> "calc=5 5"
-	* "calc= "str" "str" dup" -> "calc=str str"
-
-It duplicates the top value of the stack. You can use dup to use some value without discarding it.
-`,
-	swap: aliases => `
-
-	SWAP
-
-Usage: "calc= a b swap", where "a" and "b" can be any value.
-
-Aliases: ${aliases}.
-
-Examples:
-	* "calc= 2 5 swap" -> "calc=5 2"
-	* "calc= "gh" [1, 2, 3] swap" -> "calc=[1, 2, 3] gh"
-
-It swaps the top 2 values of the stack. This means you can use a value underneath another.
-`,
-	stack_reversen: aliases => `
-
-	REVERSE TOP N ITEMS
-
-${built_in_warning}
-
-Usage: "calc= n ... m num stack_reversen", where "num" is the amount of items you want to reverse and "n ... m" are the items.
-
-Aliases: ${aliases}.
-
-Examples:
-	* "calc= 'a 'd 'y 'f 4 stack_reversen" -> "calc=f y d a"
-	* "calc= 10 6 .. expl 1 5 .. expl 10 stack_reversen" -> "calc= 5 4 3 2 1 6 7 8 9 10"
-
-It reverses the top num items of the stack.
-`,
-	drop: aliases => `
-
-	DROP
-
-Usage: "calc= a drop", where "a" is any value.
-
-Aliases: ${aliases}.
-
-Examples:
-	* "calc= 1 2 3 drop" -> "calc=1 2"
-	* "calc= 'a 'b [] drop" -> "calc=a b"
-
-It removes the top item of the stack.
-`,
-	dropn: aliases => `
-
-	DROP N ITEMS
-
-${built_in_warning}
-
-Usage: "calc= n ... m num dropn", where "num" is the amount of items you want to drop and "n ... m" are the items.
-
-Aliases: ${aliases}.
-
-Examples:
-	* "calc= 'a 'b 'c 'd 2 dropn" -> "calc=a b"
-	* "calc= 10 1 .. expl 5 dropn" -> "calc=10 9 8 7 6"
-
-It drops the top num items of the stack. It should be called with a known, fixed number or you could remove the wrong items, which would cause very strange bugs.
-`,
-	rot: aliases => `
-
-	ROTATE
-
-Usage: "calc= a b c rot", where "a", "b", and "c" can be any value.
-
-Aliases: ${aliases}.
-
-Examples:
-	* "calc= 'a 'b 'c rot" -> "calc=c a b"
-	* "calc= "as" [] 1 rot" -> "calc=1 as []"
-
-It rotates the top three items of the stack to the right. To do this with more items you would use roll, but it is not recommended.
-`,
-	unrot: aliases => `
-
-	ROTATE BACKWARD
-
-Usage: "calc= a b c unrot", where "a", "b", and "c" can be any value.
-
-Aliases: ${aliases}.
-
-Examples:
-	* "calc= 'a 'b 'c unrot" -> "calc=b c a"
-	* "calc= "as" [] 1 unrot" -> "calc=[] 1 as"
-
-It rotates the top three items of the stack to the left. To do this with more items you would use unroll, but it is not recommended.
-`,
-	roll: aliases => `
-
-	ROLL
-
-Usage: "calc= n ... m num roll", where "num" is the amount of items you want to roll and "n ... m" are the items.
-
-Aliases: ${aliases}.
-
-Examples:
-	* "calc= 'a 'b 'c 'd 'e 'f 4 roll" -> "calc=a b f c d e"
-	* "calc= 1 5 .. {n -> 1 n .. expl n roll n group} map" -> "calc=[[1], [2, 1], [3, 1, 2], [4, 1, 2, 3], [5, 1, 2, 3, 4]]"
-
-It rotates the top num items of the stack to the left. If you want to roll 2 or 3 three items, please check out swap and rot.
-`,
-	unroll: aliases => `
-
-	ROLL BACKWARD
-
-Usage: "calc= n ... m num unroll", where "num" is the amount of items you want to roll backward and "n ... m" are the items.
-
-Aliases: ${aliases}.
-
-Examples:
-	* "calc= 'a 'b 'c 'd 'e 'f 4 unroll" -> "calc=a b d e f c"
-	* "calc= 1 5 .. {n -> 1 n .. expl n unroll n group} map" -> "calc=[[1], [2, 1], [2, 3, 1], [2, 3, 4, 1], [2, 3, 4, 5, 1]]"
-
-It rotates the top num items of the stack to the right. If you want to roll backward 2 or 3 three items, please check out swap and unrot.
-`,
-/*
-	nip
-	tuck
-	over
-	map
-	fold
-	foldr
-	filter
-	length
-	head
-	snd
-	last
-	back_snd
-	nth
-	back_nth
-	init
-	tail
-	body
-	reverse
-	reversen
-	popn
-	expl
-	group
-	copy_group
-	group_all
-	copy_group_all
-	list_dup
-	list_swap
-	list_rot
-	list_unrot
-	list_roll
-	list_reverse_roll
-	list_nip
-	list_tuck
-	list_over
-	pi
-	e
-	abs
-	round
-	ceil
-	floor
-	max
-	main
-	sgn
-	rand
-	cos
-	sin
-	tan
-	sqrt
-	cbrt
-	root
-	log
-	call
-	iter
-	id
-	dot
-	set
-	inc
-	dec*/
-};
-
-var built_in_warning = "!WARNING: This function is discouraged from being used, the only reason it is here is for the few cases in which it is necessary!";
-
 // Taken from https://stackoverflow.com/questions/14743536/multiple-key-names-same-pair-value .
 function expand(obj) {
 	var keys = Object.keys(obj);
@@ -1674,7 +1675,7 @@ Object.compare = function(obj1, obj2) {
 };
 
 module.exports = {built_ins, operators};
-},{"./print":3,"./run part":4,"./types":7,"./variable manipulation":8}],7:[function(require,module,exports){
+},{"./help pages":1,"./print":4,"./run part":5,"./types":8,"./variable manipulation":9}],8:[function(require,module,exports){
 var types = {
 	num: 0, str: 1, list: 2,
 	func: 3, sym: 4, op: 5
@@ -1698,7 +1699,7 @@ function type_to_str(type) {
 }
 
 module.exports = {...types, type_to_str};
-},{}],8:[function(require,module,exports){
+},{}],9:[function(require,module,exports){
 function get_variable(name, scopes) {
 	for(let cou = scopes.length - 1; cou >= 0; cou--) {
 		if(scopes[cou][name]) {
@@ -1746,4 +1747,4 @@ var err_to_str = err => !(err instanceof Error)
 	: "\n" + err.stack.split("\n").slice(0, 2).join("\n\t");
 
 module.exports = {calc, print, err_to_str};
-},{"./lex":1,"./parse":2,"./print":3,"./run":5}]},{},[]);
+},{"./lex":2,"./parse":3,"./print":4,"./run":6}]},{},[]);
