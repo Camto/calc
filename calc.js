7f948bc608f9640fb10852a17d7f4427c5aabf91
Benjamin Philippe Applegate <www.ben.pr@gmail.com>
2019-02-11 15:03:05 -0400

    Running empty blocks forever now stops.

--- calc.js
+++ calc.js
@@ -1588,6 +1588,9 @@ function run_function(func, stack, built_ins, operators, end_time) {
 }
 
 function run_block(block, stack, scopes, built_ins, operators, end_time) {
+	if(Date.now() > end_time) {
+		throw "Error: code took too long to run, stopped.";
+	}
 	for(let instruccion_pointer = 0; instruccion_pointer < block.length; instruccion_pointer++) {
 		if(Date.now() > end_time) {
 			throw "Error: code took too long to run, stopped.";
