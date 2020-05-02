3ecbca0cd95a0152ab28131b80e2892106303475
Camto <www.ben.pr@gmail.com>
2020-05-02 02:09:03 -0400

    Change from discord.io to discord.js

--- Discord/calcbot.js
+++ Discord/calcbot.js
@@ -1,38 +1,31 @@
 "use strict";
 
 var calc = require("./../calc/calc");
-
-var Discord = require("discord.io");
-var logger = require("winston");
 var auth = require("./auth.json");
+var discord = require("discord.js");
 
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-	colorize: true
-});
-logger.level = "debug";
+var client = new discord.Client();
 
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+client.on("ready", () => {
+	console.log(`${client.user.tag} is logged in!`);
 });
 
-bot.on("ready", function (evt) {
-	logger.info(`${bot.username} is logged in!`);
-});
-
-bot.on("message", function(user, user_id, channel_id, message, event) {
-	if(user != "calcbot" && message.substring(0, 5) == "calc=") {
-		bot.sendMessage({
-			to: channel_id,
-			message: (() => {
-				try {
-					return calc.print(calc.calc(message));
-				} catch(err) {
-					console.log(calc.err_to_str(err));
-					return calc.err_to_str(err);
-				}
-			})()
-		});
+client.on("message", msg => {
+	if(
+		msg.author.username != "calcbot" &&
+		msg.content.substring(0, 5) == "calc="
+	) {
+		var result = (() => {
+			try {
+				return calc.print(calc.calc(msg.content));
+			} catch(err) {
+				console.log(calc.err_to_str(err));
+				return calc.err_to_str(err);
+			}
+		})();
+		
+		msg.channel.send(result);
 	}
 });
+
+client.login(auth.token);
