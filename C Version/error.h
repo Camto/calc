9297c699492f825f35510abec3f982ce089c94f8
Camto <www.ben.pr@gmail.com>
2020-07-08 02:33:18 -0400

    Start `error.c`.

---
+++ C Version/error.h
@@ -0,0 +1,18 @@
+#ifndef CALC_ERROR
+#define CALC_ERROR
+
+#include "len str.h"
+
+typedef Calc_Len_Str* Calc_Maybe_Error;
+
+typedef enum {calc_error, calc_result} Calc_Is_Error;
+#define Calc_Errorful(T) \
+	struct { \
+		Calc_Is_Error is_error; \
+		union { \
+			Calc_Len_Str* error; \
+			T result; \
+		} data; \
+	}
+
+#endif
