e3f927b0248888bbe0ff531747d218e1f698c1ee
Camto <www.ben.pr@gmail.com>
2020-07-08 18:57:55 -0400

    Make calcbot ignore all bots now.

--- C Version/parse.h
+++ C Version/parse.h
@@ -1,6 +1,8 @@
 #ifndef CALC_PARSE
 #define CALC_PARSE
 
+#include <stdlib.h>
+
 #include "lex.h"
 
 typedef struct Calc_Ast_Func Calc_Ast_Func;
@@ -46,14 +48,16 @@ struct Calc_Ast_List {
 	Calc_Ast_Expr** items;
 };
 
-Calc_Ast_Func calc_parse(const Calc_Tokens tokens);
+Calc_Ast calc_parse(const Calc_Tokens tokens);
 
-Calc_Ast_Func calc_parse_prog(const Calc_Tokens* tokens size_t* pos);
+Calc_Ast calc_parse_prog(const Calc_Tokens* tokens size_t* pos);
 Calc_Ast_Var calc_parse_var(const Calc_Tokens* tokens size_t* pos);
 Calc_Ast_Expr calc_parse_expr(const Calc_Tokens* tokens size_t* pos);
 Calc_Ast_Instr calc_parse_instr(const Calc_Tokens* tokens size_t* pos);
 Calc_Ast_Func calc_parse_func(const Calc_Tokens* tokens size_t* pos);
 Calc_Ast_List calc_parse_list(const Calc_Tokens* tokens size_t* pos);
 Calc_Ast_Instr calc_parse_refof(const Calc_Tokens* tokens size_t* pos);
+
+void calc_free_ast(Calc_Ast ast);
 
 #endif
