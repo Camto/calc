81885c0a232d6d8fa0428faf847d823b9edf49ad
Camto <www.ben.pr@gmail.com>
2020-07-09 19:35:11 -0400

    Make barebones CLI.

---
+++ CLI/calc.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var fs = require("fs");
+var calc = require("./../calc/calc");
+
+var args = process.argv.slice(2);
+
+// Running exprs directly from the command line can be added later.
+if(args.length >= 1) {
+	var prog = fs.readFileSync(args[0]);
+	var result;
+	var result = (() => {
+		try {
+			return calc.print(calc.calc("calc= " + prog));
+		} catch(err) {
+			console.log(`Ran program
+${prog}
+with error
+${calc.err_to_str(err)}`);
+			return calc.err_to_str(err);
+		}
+	})();
+	console.log(result);
+} else {
+	console.log("calc=Error: did not provide file to read.");
+}
