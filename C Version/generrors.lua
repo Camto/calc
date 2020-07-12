a406a6e074332c3ebbe7b1e31b7a13ef84681a92
Camto <www.ben.pr@gmail.com>
2020-07-12 16:57:20 -0400

    Generate function, it's string, and prototype.

--- C Version/generrors.lua
+++ C Version/generrors.lua
@@ -15,10 +15,10 @@ local str, insert = false, true
 local function parse_str(prog, i)
 	local str_ = prog:sub(i):match"`[^`]*`":sub(2, -2)
 	
-	local parsed_str = {}
+	local parsed_str = List{}
 	local current_state = str
 	for section in str_:gmatch"[^\\]+" do
-		table.insert(parsed_str, {type = current_state, data = section})
+		parsed_str:append{type = current_state, data = section}
 		current_state = not current_state
 	end
 	
@@ -37,19 +37,47 @@ local function parse_prog(prog)
 		i = skip_ws(prog, i)
 		str_, i = parse_str(prog, i)
 		i = skip_ws(prog, i)
-		table.insert(errors, {name = name, str = str_})
+		errors:append{name = name, str = str_}
 	end
 	
 	return errors
 end
 
-local function error_to_func(error)
-	return
-"Calc_Len_Str* " .. error.name .. [[(const Calc_Len_Str* prog, size_t pos) {
-	Calc_Len_Str* buf = calc_error_header(prog, pos);
-}
+local function to_c_char(char)
+	if char == "'" then return "'\\''"
+	elseif char == "\n" then return "'\\n'"
+	elseif char == "\\" then return "'\\\\'"
+	else return "'" .. char .. "'" end
+end
 
-]]
+local function error_to_func(error)
+	local name = error.name
+	local str = error.str[1].data
+	
+	return {
+		"static const Calc_Len_Str_Size(" .. #str .. ") calc_error_" .. name ..
+		"_str = {" .. #str .. ", " .. List(str):map(to_c_char):join", " .. "};",
+		
+"Calc_Len_Str* calc_error_" .. name .. [[(const Calc_Len_Str* prog, size_t pos) {
+	Calc_Len_Str* buf;
+	Calc_Error_Header_Info info = Calc_Error_Header_Info(prog, pos);
+	buf = calc_len_str_prealloc(
+		(info.start_cut - info.end_cut) * 2 + 3 + ]] .. #str .. [[
+
+	);
+	calc_error_append_header(
+		buf,
+		prog, pos,
+		info.start_cut, info.end_cut
+	);
+	calc_len_str_append(buf, calc_two_newlines);
+	calc_len_str_append(buf, calc_error_]] .. name .. [[_str);
+	
+	return buf;
+}]],
+		
+		"Calc_Len_Str* calc_error_" .. name .. "(const Calc_Len_Str* prog, size_t pos);"
+	}
 end
 
 local error_file = io.open("error.err", "r")
@@ -58,4 +86,4 @@ error_file:close()
 
 errors = parse_prog(prog)
 
-print(errors:map(error_to_func))
+pretty.dump(errors:map(error_to_func))
